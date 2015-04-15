0724499391aba9a2e1e8b3db12263429c7cab5a9
Joe Hildebrand <joe+github@cursive.net>
2015-04-15 15:17:52 -0600

    The proposed changes
    A proposed fix for issue #6.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ module.exports = function(str, options) {
 
   var newline = options.newline || '\n' + indent;
 
-  var re = new RegExp('.{1,' + width + '}(\\s|$)|\\S+?(\\s|$)', 'g');
+  var re = new RegExp('.{1,' + width + '}(\\s+|$)|\\S+?(\\s+|$)', 'g');
   var lines = str.match(re) || [];
   var res = indent + lines.join(newline);
 
