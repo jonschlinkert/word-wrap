857e7a93e3e0326b218a999dfc937f9406973610
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-19 23:53:47 -0400

    lint

--- test.js
+++ test.js
@@ -7,10 +7,10 @@
 
 'use strict';
 
+/* deps:mocha */
+require('should');
 var assert = require('assert');
-var should = require('should');
 var wrap = require('./');
-
 
 var str = 'A project without documentation is like a project that doesn\'t exist. Verb solves this by making it dead simple to generate project documentation, using simple markdown templates, with zero configuration required.        ';
 
