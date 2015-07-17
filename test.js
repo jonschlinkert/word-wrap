afd6aa02ed184d6441fa2b879a55d1820168e457
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-17 12:09:55 -0400

    lint

--- test.js
+++ test.js
@@ -1,14 +1,6 @@
-/*!
- * word-wrap <https://github.com/jonschlinkert/word-wrap>
- *
- * Copyright (c) 2014-2015, Jon Schlinkert.
- * Licensed under the MIT License.
- */
-
 'use strict';
 
-/* deps:mocha */
-require('should');
+/* deps: mocha */
 var assert = require('assert');
 var wrap = require('./');
 
@@ -16,40 +8,40 @@ var str = 'A project without documentation is like a project that doesn\'t exist
 
 describe('wrap', function () {
   it('should use defaults to wrap words in the given string:', function () {
-    wrap(str).should.equal('  A project without documentation is like a project \n  that doesn\'t exist. Verb solves this by making it \n  dead simple to generate project documentation, \n  using simple markdown templates, with zero \n  configuration required.        ');
+    assert.equal(wrap(str), '  A project without documentation is like a project \n  that doesn\'t exist. Verb solves this by making it \n  dead simple to generate project documentation, \n  using simple markdown templates, with zero \n  configuration required.        ');
   });
 
   it('should wrap to the specified width:', function () {
-    wrap(str, {width: 40}).should.equal('  A project without documentation is like \n  a project that doesn\'t exist. Verb \n  solves this by making it dead simple to \n  generate project documentation, using \n  simple markdown templates, with zero \n  configuration required.        ');
+    assert.equal(wrap(str, {width: 40}), '  A project without documentation is like \n  a project that doesn\'t exist. Verb \n  solves this by making it dead simple to \n  generate project documentation, using \n  simple markdown templates, with zero \n  configuration required.        ');
   });
 
   it('should indent the specified amount:', function () {
-    wrap(str, {indent: '      '}).should.equal('      A project without documentation is like a project \n      that doesn\'t exist. Verb solves this by making it \n      dead simple to generate project documentation, \n      using simple markdown templates, with zero \n      configuration required.        ');
+    assert.equal(wrap(str, {indent: '      '}), '      A project without documentation is like a project \n      that doesn\'t exist. Verb solves this by making it \n      dead simple to generate project documentation, \n      using simple markdown templates, with zero \n      configuration required.        ');
   });
 
   it('should use the given string for newlines:', function () {
-    wrap(str, {newline: '\n\n-'}).should.equal('  A project without documentation is like a project \n\n-that doesn\'t exist. Verb solves this by making it \n\n-dead simple to generate project documentation, \n\n-using simple markdown templates, with zero \n\n-configuration required.        ');
+    assert.equal(wrap(str, {newline: '\n\n-'}), '  A project without documentation is like a project \n\n-that doesn\'t exist. Verb solves this by making it \n\n-dead simple to generate project documentation, \n\n-using simple markdown templates, with zero \n\n-configuration required.        ');
   });
 
   it('should trim trailing whitespace:', function () {
-    wrap(str, {trim: true}).should.equal('  A project without documentation is like a project\n  that doesn\'t exist. Verb solves this by making it\n  dead simple to generate project documentation,\n  using simple markdown templates, with zero\n  configuration required.');
+    assert.equal(wrap(str, {trim: true}), '  A project without documentation is like a project\n  that doesn\'t exist. Verb solves this by making it\n  dead simple to generate project documentation,\n  using simple markdown templates, with zero\n  configuration required.');
   });
 
   it('should handle strings with just newlines', function () {
-    wrap('\r\n', {indent: '\r\n', width: 18}).should.equal('\r\n');
+    assert.equal(wrap('\r\n', {indent: '\r\n', width: 18}), '\r\n');
   });
 
   it('should handle strings that break where there are multiple spaces', function() {
-    wrap('foo foo.  bar', {width:8}).should.equal('  foo foo.  \n  bar');
-    wrap('foo foo.  bar', {width:8, trim: true}).should.equal('  foo foo.\n  bar');
+    assert.equal(wrap('foo foo.  bar', {width:8}), '  foo foo.  \n  bar');
+    assert.equal(wrap('foo foo.  bar', {width:8, trim: true}), '  foo foo.\n  bar');
   });
 
   it('should cut one long word', function() {
-    wrap('Supercalifragilisticexpialidocious', {width:24, cut:true}).should.equal('  Supercalifragilisticexpi\n  alidocious');
+    assert.equal(wrap('Supercalifragilisticexpialidocious', {width:24, cut:true}), '  Supercalifragilisticexpi\n  alidocious');
   });
 
   it('should cut long words', function() {
-    wrap('Supercalifragilisticexpialidocious and Supercalifragilisticexpialidocious', {width:24, cut:true}).should.equal('  Supercalifragilisticexpi\n  alidocious and Supercali\n  fragilisticexpialidociou\n  s');
+    assert.equal(wrap('Supercalifragilisticexpialidocious and Supercalifragilisticexpialidocious', {width:24, cut:true}), '  Supercalifragilisticexpi\n  alidocious and Supercali\n  fragilisticexpialidociou\n  s');
   })
 
 });
