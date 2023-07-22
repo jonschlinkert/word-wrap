98943154855b0dd79b707462b9202614990c7f61
Brian Woodward <brian.woodward@gmail.com>
2023-07-22 10:33:56 -0400

    revert default indent
    Reverts the default indent to be 2 spaces. This will be published as a patch to 1.2.*

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ module.exports = function(str, options) {
   var width = options.width || 50;
   var indent = (typeof options.indent === 'string')
     ? options.indent
-    : '';
+    : '  ';
 
   var newline = options.newline || '\n' + indent;
   var escape = typeof options.escape === 'function'
